fe9807bd08a844aa810d4eec172bc98aaadd47ef
supportdcbindia <support@dcbindia.in>
2026-08-26 16:30:25 +0530

    COde commit

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="main-footer footer">
+<!-- <footer class="main-footer footer">
     <div class="upper-box">
         <div class="auto-container">
             <div class="row">
@@ -80,8 +80,198 @@
         </div>
     </div>
 </div>
-</div>
+</div> -->
+<footer class="main-footer footer footer-modern">
 
+    <!-- Top CTA -->
+    <div class="footer-top-cta">
+        <div class="auto-container">
+            <div class="footer-top-cta-inner">
+                <div>
+                    <span>LET'S BUILD SOMETHING RELIABLE</span>
+                    <h3>Looking for Lighting Poles, Cable Trays or Custom Engineering Solutions?</h3>
+                </div>
+
+                <a href="contact-us.html" class="footer-cta-btn">
+                    Get In Touch
+                    <i class="fa fa-arrow-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Main Footer -->
+    <div class="upper-box">
+        <div class="auto-container">
+
+            <div class="row">
+
+                <!-- About -->
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="widget about-widget">
+
+                        <h4 class="widget_title">About JETCOTECH</h4>
+
+                        <div class="text">
+                            JETCOTECH is a Manufacturer, Supplier &amp; Exporter
+                            with a strong presence in Cable Trays, Lighting Poles,
+                            High Mast Poles, Earthing Solutions, Solar Structures
+                            &amp; Hot Dip Galvanizing.
+                        </div>
+
+                        <div class="footer-social-title">
+                            Follow Us
+                        </div>
+
+                        <ul class="social-links">
+                            <li>
+                                <a href="https://www.facebook.com/JetcotechEngineeringLLP">
+                                    <i class="fa fa-facebook-f"></i>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="https://www.instagram.com/jetcotechengineering/">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="https://www.linkedin.com/company/jetcotech-engineering">
+                                    <i class="fa fa-linkedin"></i>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="https://www.youtube.com/@jetcotechengineering2226">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+
+
+                <!-- Quick Links -->
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="widget links-widget">
+
+                        <h4 class="widget_title">Quick Links</h4>
+
+                        <div class="widget-content">
+                            <div class="row">
+
+                                <div class="col-md-6">
+                                    <ul class="list">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about-us.html">About Us</a></li>
+                                        <li><a href="product.php">Products</a></li>
+                                        <li><a href="catalogue.html">Catalogue</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <ul class="list">
+                                        <li><a href="clients.html">Clients</a></li>
+                                        <li><a href="facility.html">Facilities</a></li>
+                                        <li><a href="contact-us.html">Contact Us</a></li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <!-- Contact -->
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="contact-widget widget">
+
+                        <h4 class="widget_title">Get In Touch</h4>
+
+                        <ul class="footer-contact-list">
+
+                            <li>
+                                <div class="footer-contact-icon">
+                                    <i class="fa fa-map-marker"></i>
+                                </div>
+
+                                <div>
+                                    <span>Address</span>
+                                    <p>
+                                        Opp. Old Pipli Bus-Station,<br>
+                                        Jetpar Road, Morbi-363642,<br>
+                                        Gujarat, India
+                                    </p>
+                                </div>
+                            </li>
+
+
+                            <li>
+                                <div class="footer-contact-icon">
+                                    <i class="fa fa-phone"></i>
+                                </div>
+
+                                <div>
+                                    <span>Call Us</span>
+                                    <p>
+                                        <a href="tel:+917575007163">+91 75750 07163</a><br>
+                                        <a href="tel:+916351897900">+91 63518 97900</a>
+                                    </p>
+                                </div>
+                            </li>
+
+
+                            <li>
+                                <div class="footer-contact-icon">
+                                    <i class="fa fa-envelope"></i>
+                                </div>
+
+                                <div>
+                                    <span>Email Us</span>
+                                    <p>
+                                        <a href="mailto:sales@jetcotech.in">sales@jetcotech.in</a><br>
+                                        <a href="mailto:marketing@jetcotech.in">marketing@jetcotech.in</a>
+                                    </p>
+                                </div>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- Bottom Footer -->
+    <div class="footer-bottom">
+        <div class="auto-container">
+
+            <div class="footer-bottom-inner">
+
+                <p>
+                    © <?php echo date('Y'); ?> JETCOTECH Engineering. All Rights Reserved.
+                </p>
+
+                <div class="footer-bottom-links">
+                    <a href="privacy-policy.html">Privacy Policy</a>
+                    <a href="terms-condition.html">Terms &amp; Conditions</a>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</footer>
 
 <div id="help-button">
     <a href="https://api.whatsapp.com/send?phone=917575007163" target="_blank">
