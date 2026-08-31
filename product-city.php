6011652181ab17ba7c775fa1392a97ce585f1cef
supportdcbindia <support@dcbindia.in>
2026-08-31 12:43:14 +0530

    Update product-city.php

--- product-city.php
+++ product-city.php
@@ -459,15 +459,15 @@
         ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT); ?>
     </script>
 
-    <link href="./css/bootstrap.css" rel="stylesheet">
-    <link href="./js/slider/ubislider.css" rel="stylesheet" type="text/css">
-    <link href="./css/style.css" rel="stylesheet">
-    <link href="./css/responsive.css" rel="stylesheet">
-    <link href="./css/model-custom.css" rel="stylesheet">
-    <link href="./css/sidebar-btn.css" rel="stylesheet">
+    <link src="/css/bootstrap.css" rel="stylesheet">
+    <link src="/js/slider/ubislider.css" rel="stylesheet" type="text/css">
+    <link src="/css/style.css" rel="stylesheet">
+    <link src="/css/responsive.css" rel="stylesheet">
+    <link src="/css/model-custom.css" rel="stylesheet">
+    <link src="/css/sidebar-btn.css" rel="stylesheet">
 
-    <link rel="shortcut icon" href="./images/favicon.png" type="image/x-icon">
-    <link rel="icon" href="./images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" src="/images/favicon.png" type="image/x-icon">
+    <link rel="icon" src="/images/favicon.png" type="image/x-icon">
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
