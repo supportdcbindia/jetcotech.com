b69c5ae7129f00a177ada1671bf21c4b392ed010
supportdcbindia <support@dcbindia.in>
2026-09-26 19:03:59 +0530

    Update inquiry-action.php

--- mast-poles-manufacturer/inquiry-action.php
+++ mast-poles-manufacturer/inquiry-action.php
@@ -6,6 +6,10 @@ error_reporting(0);
 // header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 // header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+echo "<pre>"; print_r($_POST);
+echo "<pre>"; print_r($_REQUEST);
+exit;
+
 // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 //     http_response_code(200);
 //     exit();
