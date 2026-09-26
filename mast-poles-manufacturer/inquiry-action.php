29492663beaec6de0a90da075e6f7753bd2ca6af
supportdcbindia <support@dcbindia.in>
2026-09-26 16:31:47 +0530

    Update inquiry-action.php

--- mast-poles-manufacturer/inquiry-action.php
+++ mast-poles-manufacturer/inquiry-action.php
@@ -2,6 +2,9 @@
 
 // ================== ERROR + HEADERS ==================
 error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 // header("Access-Control-Allow-Headers: Content-Type, Authorization");
