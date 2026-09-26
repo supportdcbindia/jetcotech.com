19c7aa58a81ec961137162f01b24a4925175bec4
supportdcbindia <support@dcbindia.in>
2026-09-26 18:33:10 +0530

    Update inquiry-action.php

--- mast-poles-manufacturer/inquiry-action.php
+++ mast-poles-manufacturer/inquiry-action.php
@@ -27,6 +27,7 @@ fwrite($myfile, json_encode($_POST));
 // ================== API SPAM CHECK ==================
 function send_request($data)
 {
+  return true;
   $curl = curl_init();
   curl_setopt_array($curl, array(
     CURLOPT_URL => 'https://dcbindia.in/akismetcurl/akismet_check.php',
@@ -69,11 +70,7 @@ $curlArr['save'] = false;
 
 $response = send_request($curlArr);
 
-echo "<pre>"; print_r($response);
-echo "<pre>"; print_r($_POST);
-exit;
-
-if ($response->result) {
+if ($response == false) {
   $curlArr['save'] = true;
   $curlArr['bcoz'] = "API FAIL";
   $curlArr['status'] = "FAIL";
